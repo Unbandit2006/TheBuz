c0497eadc688097cb9d9667c6d0cb89bf72892b6
Daniel Z <Daniel.Zheleznov@gmail.com>
2022-05-26 12:14:44 -0400

    Rewrote everything
    New Weather API, new DB
    
    API:WeatherAPI
    
    DB: Firestore Realtime DB

---
+++ GetWeatherData.py
@@ -0,0 +1,58 @@
+# This is the file that will get data from the WeatherAPI
+
+import json
+import requests
+import time
+
+class WeatherData:
+	def __init__(self, APIKey, Zipcode, Hours):
+		self.APIKey = APIKey
+		self.Zipcode = Zipcode
+		self.Hours = Hours
+		self.Message = None
+
+	def GetWeatherData(self):
+
+		Request = requests.request("GET", f"https://api.weatherapi.com/v1/forecast.json?key={self.APIKey}&q={self.Zipcode}&days=1").text
+		Content = json.loads(Request)
+		
+		LocalTime = time.time()
+		LocalTime = time.localtime(LocalTime)
+		LocalHour = LocalTime.tm_hour
+
+		ListofHours = Content['forecast']['forecastday'][0]['hour']
+
+		HoursToSearch = []
+		for i in range(self.Hours+1):
+			HoursToSearch.append(LocalHour + i)
+
+		
+		Message = [rf"The Weather for the next {self.Hours} hours for Zipcode {self.Zipcode}:\n\n"]
+		for Hour in HoursToSearch:
+			Time = ListofHours[Hour]['time'][11:16]
+			Temperature = ListofHours[Hour]['temp_f']
+			FeelsLike = ListofHours[Hour]['feelslike_f']
+			BriefForecast = ListofHours[Hour]['condition']['text']
+
+			if int(Time[:2]) > 12:
+				Hour = int(Time[:2]) - 12
+				Message.append(rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
+
+			elif int(Time[:2]) == 12:
+				Hour = int(Time[:2])
+				Message.append(rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")				
+
+			else:
+				Hour = int(Time[:2])
+				Message.append(rf"At {Hour}:{Time[3:]} AM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
+
+		
+		self.Message = "".join(Message)
+
+		return self.Message
+
+if __name__ == '__main__':
+	thingy = WeatherData("016e19390dba4ad5807184556222205", 10306, 2)
+	Message = thingy.GetWeatherData()
+	print(Message)
+
---
+++ Main.py
@@ -0,0 +1,30 @@
+# Main File that get user data and blasts it
+import firebase_admin as admin
+from firebase_admin import db
+import time
+import GetWeatherData
+import pytextnow
+
+Credentials = admin.credentials.Certificate("Firebase-sdk.json")
+admin.initialize_app(Credentials, {"databaseURL":"https://beeper5252022-default-rtdb.firebaseio.com/"})
+
+UserRef = db.reference('Users')
+UserData = UserRef.get()
+
+CurrentTime = time.time()
+CurrentTime = time.localtime(CurrentTime)
+CurrentHour = CurrentTime.tm_hour
+CurrentMin = CurrentTime.tm_min
+
+Messenger = pytextnow.Client('daniel.zheleznov', 's%3AAzwc-q8e75aJ_a4eWuPrN-K324IiXIWl.jRRw9gH%2FIpyENQ4yxzKTaKsLspUWQmS0j39Z6nSbuEc', 's%3AYGxOhxYPJKW6HUv9sY8e4jH7.%2FyGJf1oP9YNycMOjQfk%2FqjtOoI4yU3GKbZVD1DDRfE8')
+
+for User in UserData:
+	Time = UserData[User]['time']
+	Zipcode = UserData[User]['zipcode']
+	AmntOfHours = UserData[User]['amntOfHours']
+	Number = UserData[User]['number']
+	Time = Time.split(":")
+
+	if int(Time[0]) == CurrentHour and int(Time[1]) == CurrentMin:
+		Message = GetWeatherData.WeatherData("016e19390dba4ad5807184556222205", Zipcode, AmntOfHours).GetWeatherData()
+		Messenger.send_sms(str(Number), Message)
