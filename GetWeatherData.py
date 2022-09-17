43d65f26c4e39b0e6d87123b670854b3981325ba
Daniel Z <Daniel.Zheleznov@gmail.com>
2022-09-16 23:57:20 -0400

    Reformated files. Changed very little.
    TODO List:
    - Search for an alternative database.
    - Possibly Migrate to different language.

--- GetWeatherData.py
+++ GetWeatherData.py
@@ -4,52 +4,55 @@ import json
 import requests
 import time
 
+
 class WeatherData:
-	def __init__(self, APIKey, Zipcode, Hours):
-		self.APIKey = APIKey
-		self.Zipcode = Zipcode
-		self.Hours = Hours
-		self.Message = None
+    def __init__(self, APIKey, Zipcode, Hours):
+        self.APIKey = APIKey
+        self.Zipcode = Zipcode
+        self.Hours = Hours
+        self.Message = None
 
-	def GetWeatherData(self):
+    def GetWeatherData(self):
 
-		Request = requests.request("GET", f"https://api.weatherapi.com/v1/forecast.json?key={self.APIKey}&q={self.Zipcode}&days=1").text
-		Content = json.loads(Request)
-		
-		LocalTime = time.time()
-		LocalTime = time.localtime(LocalTime)
-		LocalHour = LocalTime.tm_hour
+        Request = requests.request("GET",
+                                   f"https://api.weatherapi.com/v1/forecast.json?key={self.APIKey}&q={self.Zipcode}&days=1").text
+        Content = json.loads(Request)
 
-		ListofHours = Content['forecast']['forecastday'][0]['hour']
+        LocalTime = time.time()
+        LocalTime = time.localtime(LocalTime)
+        LocalHour = LocalTime.tm_hour
 
-		HoursToSearch = []
-		for i in range(self.Hours+1):
-			HoursToSearch.append(LocalHour + i)
+        ListofHours = Content['forecast']['forecastday'][0]['hour']
 
-		
-		Message = [rf"The Weather for the next {self.Hours} hours for Zipcode {self.Zipcode}:\n\n"]
-		for Hour in HoursToSearch:
-			if int(Hour) >= 24:
-				Hour = Hour - 24
+        HoursToSearch = []
+        for i in range(self.Hours + 1):
+            HoursToSearch.append(LocalHour + i)
 
-			Time = ListofHours[Hour]['time'][11:16]
-			Temperature = ListofHours[Hour]['temp_f']
-			FeelsLike = ListofHours[Hour]['feelslike_f']
-			BriefForecast = ListofHours[Hour]['condition']['text']
+        Message = [rf"The Weather for the next {self.Hours} hours for Zipcode {self.Zipcode}:\n\n"]
+        for Hour in HoursToSearch:
+            if int(Hour) >= 24:
+                Hour = Hour - 24
 
-			if int(Time[:2]) > 12:
-				Hour = int(Time[:2]) - 12
-				Message.append(rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
+            Time = ListofHours[Hour]['time'][11:16]
+            Temperature = ListofHours[Hour]['temp_f']
+            FeelsLike = ListofHours[Hour]['feelslike_f']
+            BriefForecast = ListofHours[Hour]['condition']['text']
 
-			elif int(Time[:2]) == 12:
-				Hour = int(Time[:2])
-				Message.append(rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")				
+            if int(Time[:2]) > 12:
+                Hour = int(Time[:2]) - 12
+                Message.append(
+                    rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
 
-			else:
-				Hour = int(Time[:2])
-				Message.append(rf"At {Hour}:{Time[3:]} AM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
+            elif int(Time[:2]) == 12:
+                Hour = int(Time[:2])
+                Message.append(
+                    rf"At {Hour}:{Time[3:]} PM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
 
-		
-		self.Message = "".join(Message)
+            else:
+                Hour = int(Time[:2])
+                Message.append(
+                    rf"At {Hour}:{Time[3:]} AM\nBrief Forecast: {BriefForecast}\nIt is {Temperature}°F, but feels like {FeelsLike}°F\n\n")
 
-		return self.Message
+        self.Message = "".join(Message)
+
+        return self.Message
--- Main.py
+++ Main.py
@@ -5,14 +5,14 @@ import time
 import GetWeatherData
 import pytextnow
 
-USERNAME = "" # Type your textnow username here
-SID_COOKIE = "" # Get your SID cookie from the textnow website and put it here
-CSRF_COOKIE = "" # Get your CSRF cookie from the textnow website and put it here
-DATABASE_URL = "" # Get your database URL from the firebase website and put it here
-APIKEY = "" # Get your API key from the weather API website and put it here
+USERNAME = ""  # Type your textnow username here
+SID_COOKIE = ""  # Get your SID cookie from the textnow website and put it here
+CSRF_COOKIE = ""  # Get your CSRF cookie from the textnow website and put it here
+DATABASE_URL = ""  # Get your database URL from the firebase website and put it here
+APIKEY = ""  # Get your API key from the weather API website and put it here
 
 Credentials = admin.credentials.Certificate("Firebase-sdk.json")
-admin.initialize_app(Credentials, {"databaseURL":DATABASE_URL})
+admin.initialize_app(Credentials, {"databaseURL": DATABASE_URL})
 
 UserRef = db.reference('Users')
 UserData = UserRef.get()
@@ -25,12 +25,13 @@ CurrentMin = CurrentTime.tm_min
 Messenger = pytextnow.Client(USERNAME, SID_COOKIE, CSRF_COOKIE)
 
 for User in UserData:
-	Time = UserData[User]['time']
-	Zipcode = UserData[User]['zipcode']
-	AmntOfHours = UserData[User]['amntOfHours']
-	Number = UserData[User]['number']
-	Time = Time.split(":")
+    UserInfo = UserData[User]
+    Time = UserInfo['time']
+    Zipcode = UserInfo['zipcode']
+    AmntOfHours = UserInfo['amntOfHours']
+    Number = UserInfo['number']
+    Time = Time.split(":")
 
-	if int(Time[0]) == CurrentHour and int(Time[1]) == CurrentMin:
-		Message = GetWeatherData.WeatherData(APIKEY, Zipcode, AmntOfHours).GetWeatherData()
-		Messenger.send_sms(str(Number), Message)
+    if int(Time[0]) == CurrentHour and int(Time[1]) == CurrentMin:
+        Message = GetWeatherData.WeatherData(APIKEY, Zipcode, AmntOfHours).GetWeatherData()
+        Messenger.send_sms(str(Number), Message)
