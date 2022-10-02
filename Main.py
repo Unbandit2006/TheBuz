02130c9baa9ac2db59f9ba077cd94df060e3e8fd
Daniel Z <Daniel.Zheleznov@gmail.com>
2022-10-02 14:02:22 -0400

    Changing Database credentials because then problems

--- Main.py
+++ Main.py
@@ -9,7 +9,7 @@ USERNAME = "daniel.zheleznov"  # Type your textnow username here
 SID_COOKIE = "s%3Ap9_JYxmBC1B-YAe0kgIzhRv1DW1T8NjZ.2MJMus0dmOmI9hboz6c2MJERrZwiGx7qtPqOckVPtFg"  # Get your SID cookie from the textnow website and put it here
 CSRF_COOKIE = "s%3AV4rbcFeU2Za3InJ3kpCSpiQ2.siFz5SKHYYlCPOMneROboJXCfsHQZJk6BtRh%2B51c8fw"  # Get your CSRF cookie from the textnow website and put it here
 DATABASE_URL = "https://beeper5252022-default-rtdb.firebaseio.com/"  # Get your database URL from the firebase website and put it here
-DATABASE_CRED = "beeper5252022-firebase-adminsdk-te6kd-a403e68aff.json"  # Type what your credential filename
+DATABASE_CRED = "Beeper/beeper5252022-firebase-adminsdk-te6kd-a403e68aff.json"  # Type what your credential filename
 APIKEY = "016e19390dba4ad5807184556222205"  # Get your API key from the weather API website and put it here
 
 Credentials = admin.credentials.Certificate(f"{DATABASE_CRED}")
