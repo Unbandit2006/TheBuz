90a5b6c57a1ed90feef9ab3a6ae83caa9939f078
Daniel Zhelenzov <Daniel.Zheleznov@gmail.com>
2022-11-11 21:53:40 -0500

    Total Restructuring of Code 11-01-2022
    \nThere was a total rewrite to the code and a new message with emojis and much similer and clarifying

--- Main.py
+++ Main.py
@@ -1,61 +1,79 @@
-# Main File that get user data and blasts it
+import Program
+import configparser
 import firebase_admin as admin
 from firebase_admin import db
-import time
-import GetWeatherData
 import pytextnow
-import calendar
-import Anniversaries as Ann
 
-USERNAME = "daniel.zheleznov"  # Type your textnow username here
-SID_COOKIE = "s%3Ap9_JYxmBC1B-YAe0kgIzhRv1DW1T8NjZ.2MJMus0dmOmI9hboz6c2MJERrZwiGx7qtPqOckVPtFg"  # Get your SID cookie from the textnow website and put it here
-CSRF_COOKIE = "s%3AV4rbcFeU2Za3InJ3kpCSpiQ2.siFz5SKHYYlCPOMneROboJXCfsHQZJk6BtRh%2B51c8fw"  # Get your CSRF cookie from the textnow website and put it here
-DATABASE_URL = "https://beeper5252022-default-rtdb.firebaseio.com/"  # Get your database URL from the firebase website and put it here
-DATABASE_CRED = "beeper5252022-firebase-adminsdk-te6kd-a403e68aff.json"  # Type what your credential filename
-APIKEY = "016e19390dba4ad5807184556222205"  # Get your API key from the weather API website and put it here
+class User:
+    def __init__(self, user_info:dict):
+        self.__user = user_info
+        self.__message = ""
 
-Credentials = admin.credentials.Certificate(f"{DATABASE_CRED}")
-admin.initialize_app(Credentials, {"databaseURL": DATABASE_URL})
-
-UserRef = db.reference('Users')
-UserData = UserRef.get()
-
-CurrentTime = time.time()
-CurrentTime = time.localtime(CurrentTime)
-CurrentHour = CurrentTime.tm_hour
-CurrentMin = CurrentTime.tm_min
-CurrentDay = CurrentTime.tm_mday
-CurrentMonth = CurrentTime.tm_mon
-CurrentMonth = calendar.month_name[CurrentMonth]
-CurrentDOTW = CurrentTime.tm_wday
-CurrentDOTW = calendar.day_name[CurrentDOTW]
-CurrentYear = CurrentTime.tm_year
-
-Messenger = pytextnow.Client(USERNAME, SID_COOKIE, CSRF_COOKIE)
-Messenger.auth_reset(sid_cookie=SID_COOKIE, csrf_cookie=CSRF_COOKIE)
-
-for User in UserData:
-    UserInfo = UserData[User]
-    Name = UserInfo['fname'] + " " + UserInfo['lname']
-    Time = UserInfo['time']
-    Time = Time.split(":")
-    Zipcode = UserInfo['zipcode']
-    AmntOfHours = UserInfo['amntOfHours']
-    Number = UserInfo['number']
-
-    if int(Time[0]) == CurrentHour and int(Time[1]) == CurrentMin:
-        Message = ""
-
-        # Greetings
-        Message += rf"Hello, {Name}.\nToday is {CurrentDOTW}, {CurrentMonth} {CurrentDay}, {CurrentYear}.\n\n"
-
-        Message += GetWeatherData.WeatherData(APIKEY, Zipcode, AmntOfHours).GetWeatherData()
-        print(f"\nThe Weather Data is {Message}")
-        print(len(Message))
-
-        AnnMessage = Ann.Anniversaries(UserInfo).GetAniversariesData()
-        Message += AnnMessage
-        print(f"\nThe Anniversary Data is {AnnMessage}")
+    def get_name(self):
+        return self.__user.get("fname"), self.__user.get("lname")
+    
+    def get_number(self):
+        return str(self.__user.get("number"))
+    
+    def get_message(self):
+        return self.__message
+    
+    def create_message(self, weather_api_key:str):
+        self.__user.get("time")
+        time = Program.Time()
         
-        Messenger.send_sms(str(Number), Message)
-        print(f"\nSent To {UserInfo} at {CurrentTime}\n")
+        user_time = self.__user.get("time").split(":")
+
+        if user_time[0] == time.get_hour() and user_time[1] == time.get_minutes():
+            self.__message += rf"Today is {time.get_day_name()}, {time.get_month_name()} {time.get_day_number()}, {time.get_year()}.\n"
+            
+            if self.__user.get("weather", False):
+                weather = Program.Weather(weather_api_key, self.__user.get("zipcode"))
+                self.__message += weather.get_data()
+
+            return self.__message
+
+class FirebaseReader:
+    def __init__(self, config, mode):
+        default_cred = config.get(mode, "cred_file_location")
+        cert = admin.credentials.Certificate(default_cred)
+        admin.initialize_app(cert, options={"databaseURL":"https://beeper5252022-default-rtdb.firebaseio.com/"})
+        self.__db_ref = admin.db.reference("Users")
+
+    def get_all_usernames(self):
+        usernames = []
+        for user in self.__db_ref.get():
+            usernames.append(user)
+        
+        return usernames
+    
+    def get_value(self, key):
+        return self.__db_ref.child(key).get()        
+
+
+
+config = configparser.RawConfigParser()
+config.read("Config.ini")
+
+mode = "HOME"
+
+username = str(config.get("CONSTANTS", "username"))
+_sid_cookie = str(config.get("CONSTANTS", "sid_cookie"))
+_csrf_cookie = str(config.get("CONSTANTS", "csrf_cookie"))
+messenger = pytextnow.Client(username, _sid_cookie, _csrf_cookie)
+messenger.auth_reset(sid_cookie=_sid_cookie, csrf_cookie=_csrf_cookie)
+
+reader = FirebaseReader(config, mode)
+
+for user in reader.get_all_usernames():
+    info = reader.get_value(user)
+
+    new_user = User(info)
+    message = new_user.create_message(config.get("CONSTANTS", "weather_api_key"))
+
+    if message != None:
+        messenger.send_sms(new_user.get_number(), message)
+        print(f"Sent to {new_user.get_name()}\nMessage: '{new_user.get_message()}'")
+
+
+
