e9f9e52097b3f2f64333c5cc9883413c78581177
Daniel Zhelenzov <Daniel.Zheleznov@gmail.com>
2023-04-20 16:47:34 -0400

    [04/20/2023] [16:47] added top headlines

--- Buzzers/News.py
+++ Buzzers/News.py
@@ -3,7 +3,7 @@ import gnews
 def get_news():
     news = gnews.GNews(max_results=5)
     
-    string = ""
+    string = "\nTop Headlines\n-------------\n\n"
     for x in news.get_top_news():
         title = x["title"]
         string += f"{title}\n\n"
